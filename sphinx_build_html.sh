5618ee3b25441e954d19e14c88ba8fdbe9f3206f
cperales <cperales@uloyola.es>
2018-01-16 14:30:29 +0100

    Bash command fixed

--- sphinx_build_html.sh
+++ sphinx_build_html.sh
@@ -1,8 +1,1 @@
-Running Sphinx v1.6.6
-loading pickled environment... done
-building [mo]: targets for 0 po files that are out of date
-building [html]: targets for 0 source files that are out of date
-updating environment: 0 added, 0 changed, 0 removed
-looking for now-outdated files... none found
-no targets are out of date.
-build succeeded.
+sphinx-build source .
